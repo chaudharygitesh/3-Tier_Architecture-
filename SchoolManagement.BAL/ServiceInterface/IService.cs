d7fb3f448d95fbceb77d05d69778b1dfe4c0992c
Gitesh <chaudharygitesh270@gmail.com>
2024-07-16 18:06:39 +0530

    16 july office

---
+++ SchoolManagement.BAL/ServiceInterface/IService.cs
@@ -0,0 +1,16 @@
+﻿using SchoolManagement.BAL.Dtos;
+using SchoolManagement.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.BAL.ServiceInterface
+{
+    public interface IService
+    {
+        public Task<bool> AddPatient(PatientDto patientDetails);
+        public Task<IEnumerable<PatientDto>> GetAllPatientDetail();
+    }
+}
